ed5014d13400866e5a5d8c38e1dbf10744715f71
ehdgus2 <jdh5179@gmail.com>
2022-07-11 11:32:36 +0900

    first commit

---
+++ src/main/java/com/smart/project/web/home/act/HomeTestAct3.java
@@ -0,0 +1,66 @@
+package com.smart.project.web.home.act;
+
+import com.smart.project.component.CommonCodeComponent;
+import com.smart.project.component.data.CodeObject;
+import com.smart.project.proc.Test;
+import com.smart.project.util.CookieUtil;
+import com.smart.project.web.home.vo.JoinVO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.*;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class HomeTestAct3 {
+
+    @RequestMapping("/main")
+    public void main(){
+    }
+
+    @RequestMapping("/index")
+    public void index(){
+    }
+
+    @RequestMapping("/service")
+    public void service(){
+    }
+
+    @RequestMapping("/login")
+    public void login(){
+    }
+
+    @RequestMapping("/upload")
+    public void upload(){
+    }
+
+    @RequestMapping("/join")
+    public void join(){
+    }
+
+
+
+
+
+
+
+    @RequestMapping("/head")
+    public String head(){
+        return "fragment/head";
+
+    }
+
+
+
+}
