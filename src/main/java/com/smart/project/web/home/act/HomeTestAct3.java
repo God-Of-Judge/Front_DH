aa203c24216141c4c5ebf8b1bdd7bd2465e81012
ehdgus2 <jdh5179@gmail.com>
2022-07-21 10:22:37 +0900

    7.21 admin_main수정

--- src/main/java/com/smart/project/web/home/act/HomeTestAct3.java
+++ src/main/java/com/smart/project/web/home/act/HomeTestAct3.java
@@ -1,76 +1,133 @@
 package com.smart.project.web.home.act;
 
-import com.smart.project.component.CommonCodeComponent;
-import com.smart.project.component.data.CodeObject;
-import com.smart.project.proc.Test;
-import com.smart.project.util.CookieUtil;
-import com.smart.project.web.home.vo.JoinVO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.*;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class HomeTestAct3 {
 
-    @RequestMapping("/main")
-    public void main(){
-    }
-
-    @RequestMapping("/index")
-    public void index(){
-    }
-
-    @RequestMapping("/service")
-    public void service(){
-    }
-
-    @RequestMapping("/login")
-    public void login(){
-    }
-
-    @RequestMapping("/upload")
-    public void upload(){
-    }
-
-    @RequestMapping("/join")
-    public void join(){
-    }
-
-
-    @RequestMapping("/buttons")
-    public void buttons(){
-    }
-
-
-
-
-
+    /* 공통 영역*/
     @RequestMapping("/head")
     public String head(){
         return "fragment/head";
-
     }
-
-    // 관리자
 
     @RequestMapping("/admin_head")
     public String adminHead(){
         return "fragment/admin_head";
     }
 
+
+    @RequestMapping("/login")
+    public void login(){
+    }
+
+
+    /*========================================================================================*/
+
+    /* 참고페이지*/
+    @RequestMapping("/index")
+    public void index() {
+    }
+
+    @RequestMapping("/buttons")
+    public void buttons(){
+    }
+
+    @RequestMapping("/shop_checkout")
+    public void shop_checkout(){
+
+    }
+
+    @RequestMapping("/layout-sidenav-light2")
+    public void layoutSidenavLight2(){
+
+    }
+    @RequestMapping("/admin/layout-static2")
+    public void layoutStatic2(){
+
+    }
+
+    @RequestMapping("/register2")
+    public void register2(){
+
+    }
+
+    @RequestMapping("/icons")
+    public void icons(){
+    }
+
+
+
+    /*========================================================================================*/
+
+    /* 사용자 페이지 */
+    @RequestMapping("/main")
+    public void main(){
+
+    }
+
+    @RequestMapping("/service")
+    public void service(){
+
+    }
+
+    // 동영상 업로드
+    @RequestMapping("/upload")
+    public void upload(){
+
+    }
+
+    // 분석결과 페이지
+    @RequestMapping("/result")
+    public void result(){
+
+    }
+
+    // 회원정보수정
+    @RequestMapping("/join_edit")
+    public void join_edit(){
+
+    }
+
+    // 회원가입
+    @RequestMapping("/join")
+    public void join(){
+
+    }
+
+    // 커뮤니티
+    @RequestMapping("/community")
+    public void community(){
+
+    }
+
+    // 커뮤니티 글보기
+    @RequestMapping("/write_view")
+    public void write_view(){
+
+    }
+
+    // 마이게시판
+    @RequestMapping("/myList")
+    public void myList(){
+    }
+
+    // 글쓰기
+    @RequestMapping("/write")
+    public void write(){
+
+    }
+
+    /*========================================================================================*/
+
+    /* 관리자 페이지*/
+
+    // 메인
     @RequestMapping("admin/admin_main")
     public String adminMain(){
         return "admin/admin_main";
@@ -87,7 +144,12 @@ public class HomeTestAct3 {
     public String video_tables(){
         return "admin/video_tables";
     }
-    
-    
+
+
+
+
+
+
+
 
 }
